a7a1f7dce2781404a0ba5bf6705b2a4b9a2624e9
nguyenminhthong2020 <1612674@student.hcmus.edu.vn>
2022-04-01 18:54:02 +0700

    basic UI template for guest

---
+++ EMarket/Controllers/ProductController.cs
@@ -0,0 +1,40 @@
+﻿using EMarket.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EMarket.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly dbMarketsContext _context;
+
+        public ProductController(dbMarketsContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var p = await _context.Products.Include(p => p.Cat).FirstOrDefaultAsync(p => p.ProductId == id);
+
+            if (p == null)
+            {
+                return NotFound();
+            }
+            return View(p);
+        }
+    }
+}
